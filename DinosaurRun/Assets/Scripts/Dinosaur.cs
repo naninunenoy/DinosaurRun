95452c9accffb0aec352ed3733ae443397b236dd
nakano yosuke <naninuneno0411@gmail.com>
2018-10-20 20:07:13 +0900

    set run script

---
+++ DinosaurRun/Assets/Scripts/Dinosaur.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dinosaur : MonoBehaviour {
+    [SerializeField] float runSpeed = 1.0F;
+
+    private Animator animator;
+    private bool isRunning = false;
+    public bool IsRunning {
+        set {
+            if (isRunning != value) {
+                isRunning = value;
+                if (animator != null) {
+                    animator.SetBool("IsRunning", isRunning);
+                }
+            }
+        }
+        get {
+            return isRunning;
+        }
+    }
+
+    void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update() {
+        if (isRunning) {
+            // ひたすら右へ
+            transform.Translate(runSpeed * Vector2.right * Time.deltaTime);
+        }
+    }
+
+}
---
+++ DinosaurRun/Assets/Scripts/GameMain.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMain : MonoBehaviour
+{
+    [SerializeField] Dinosaur dinosaur;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            dinosaur.IsRunning = !dinosaur.IsRunning;
+        }
+    }
+}
