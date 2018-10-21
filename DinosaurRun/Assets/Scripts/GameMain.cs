8550522f38bd04b17e745a3c477c30f6954ae6cc
nakano yosuke <naninuneno0411@gmail.com>
2018-10-21 21:13:05 +0900

    add ground move script

--- DinosaurRun/Assets/Scripts/GameMain.cs
+++ DinosaurRun/Assets/Scripts/GameMain.cs
@@ -6,15 +6,31 @@ public class GameMain : MonoBehaviour
 {
     [SerializeField] Camera mainCamera;
     [SerializeField] Dinosaur dinosaur;
+    [SerializeField] Ground ground;
 
     void Start() {
         dinosaur.IsRunning = true;
     }
 
-    void Update()
-    {
+    void Update() {
+        // カメラをdinosaurに合わせる
+        var cameraX = MoveCamera(mainCamera, dinosaur.transform);
+        // カメラ位置がgrandをこえたらステージをスライド
+        if (cameraX > ground.Center.x) {
+            ground.AppendAndRemove();
+        }
+        // キー入力
         if (Input.GetKeyDown(KeyCode.Space)) {
             dinosaur.Jump();
         }
+    }
+
+    private float MoveCamera(Camera camera, Transform target) {
+        var current = camera.transform.position;
+        var to = new Vector3(target.position.x, current.y, current.z);
+        if (to.x > current.x) {
+            camera.transform.position = Vector3.MoveTowards(current, to, 1.0F);
+        }
+        return to.x;
     }
 }
---
+++ DinosaurRun/Assets/Scripts/Ground.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    [SerializeField] GameObject grandPrefab;
+    [SerializeField] int grandElmCount = 4;// 偶数
+
+    float elmWidth;
+    public float GrandWidth { get { return elmWidth; } }
+    public float TotalGrandWidth { get { return elmWidth * grandElmCount; } }
+    float leftEdgePos;
+    Queue<GameObject> grandQueue = new Queue<GameObject>();
+    Vector3 center = Vector3.zero;
+    public Vector3 Center { get { return center; } }
+
+    void Awake() {
+        elmWidth = grandPrefab.GetComponent<SpriteRenderer>().bounds.size.x;
+        for (int i = 0; i < transform.childCount; i++) {
+            var gra = transform.GetChild(i).gameObject;
+            grandQueue.Enqueue(gra);
+            
+        }
+        // 整列
+        AdjustGrands();
+    }
+
+    private void AdjustGrands() {
+        var xPos = grandQueue.Peek().transform.position.x;
+        int i = 0;
+        foreach (var gra in grandQueue) {
+            var current = gra.transform.position;
+            gra.transform.position = new Vector3(xPos, current.y, current.z);
+            // 中心位置の更新
+            if (grandElmCount / 2 == i) {
+                var graPos = gra.transform.position;
+                center = new Vector3(graPos.x - elmWidth / 2.0F, graPos.y, graPos.z);
+            }
+            i++;
+            xPos += elmWidth;
+        }
+    }
+
+    public void AppendAndRemove() {
+        var left = grandQueue.Dequeue();
+        var leftPos = left.transform.position;
+        var rightPos = new Vector3(leftPos.x + elmWidth * grandElmCount, leftPos.y, leftPos.z);
+        // 右端に追加
+        var right = Instantiate(grandPrefab, rightPos, Quaternion.identity, transform);
+        grandQueue.Enqueue(right);
+        // 左端を削除
+        Destroy(left);
+        AdjustGrands();
+    }
+}
